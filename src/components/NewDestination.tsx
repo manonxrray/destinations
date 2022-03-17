67e7136effc6e77c7a755d384841ab068c9d4b3c
Manon <manon.gerray@gmail.com>
2022-03-17 12:22:38 +0100

    Add new destination form components

--- src/components/NewDestination.tsx
+++ src/components/NewDestination.tsx
@@ -1,4 +1,12 @@
-import { Modal, Typography } from '@mui/material';
+import {
+  Modal,
+  Typography,
+  TextField,
+  Switch,
+  FormControlLabel,
+  InputAdornment,
+  Button
+} from '@mui/material';
 import styled from '@emotion/styled';
 
 type ModalProps = {
@@ -11,13 +19,20 @@ const ModalContent = styled.div`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  width: 400;
+  width: 650px;
   background: #fff;
-  border: 2px solid #000;
-  boxShadow: 24;
-  p: 4
+  border-radius: 10px;
+  padding: 2rem
 `;
 
+const Form = styled.form`
+  display: flex;
+  flex-direction: column
+`;
+
+const FormFooter = styled.div`
+  flex-direction: row
+`;
 
 const NewDestination = ({ close, open }: ModalProps): JSX.Element => {
   return (
@@ -27,11 +42,73 @@ const NewDestination = ({ close, open }: ModalProps): JSX.Element => {
     >
       <ModalContent>
         <Typography variant="h6" component="h2">
-          Text in a modal
+          Ajouter une nouvelle destination
         </Typography>
-        <Typography sx={{ mt: 2 }}>
-          Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-        </Typography>
+
+        <Form>
+          <TextField
+            required
+            id="filled-required"
+            label="Nom de la destination"
+            type="string"
+            variant="filled"
+          />
+          <TextField
+            required
+            id="filled-required"
+            label="Adresse"
+            type="string"
+            variant="filled"
+          />
+          <TextField
+            required
+            id="filled-required"
+            label="Lien de l'image"
+            type="string"
+            variant="filled"
+          />
+          <FormFooter>
+            <TextField
+              required
+              id="filled-number"
+              label="Nb Habitants"
+              type="number"
+              variant="filled"
+            />
+            <TextField
+              required
+              id="filled-number"
+              label="Nb Hôtels"
+              type="number"
+              variant="filled"
+            />
+            <TextField
+              required
+              id="filled-number"
+              label="Revenu Moy"
+              type="number"
+              variant="filled"
+              InputProps={{
+                endAdornment: <InputAdornment position="end">€</InputAdornment>,
+              }}
+            />
+            <TextField
+              required
+              id="filled-number"
+              label="Superficie"
+              type="number"
+              variant="filled"
+              InputProps={{
+                endAdornment: <InputAdornment position="end">km²</InputAdornment>,
+              }}
+            />
+          </FormFooter>
+          <FormControlLabel control={<Switch defaultChecked />} label="Activer" />
+          <div>
+            <Button variant="outlined">Cancel</Button>
+            <Button variant="contained">Confirm</Button>
+          </div>
+        </Form>
       </ModalContent>
     </Modal>
   )
